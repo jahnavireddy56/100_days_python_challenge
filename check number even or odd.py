0efc3dc9f82ef76747edfb5b87d002006a61f43e
Jahnavi saddala <saddalajahnavireddy@gmail.com>
2025-09-18 21:32:45 +0530

    Add files via upload

---
+++ check number even or odd.py
@@ -0,0 +1,10 @@
+# code.py
+
+# Take input from the user
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if num % 2 == 0:
+    print(num, "is even")
+else:
+    print(num, "is odd")
