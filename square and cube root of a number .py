d44cc8b6f46276b661747497d07938d5cfc699dc
Jahnavi saddala <saddalajahnavireddy@gmail.com>
2025-09-18 17:18:45 +0530

    Add files via upload

---
+++ square and cube root of a number .py
@@ -0,0 +1,12 @@
+# code.py
+
+# Take a number as input
+num = int(input("Enter a number: "))
+
+# Calculate square and cube
+square = num ** 2
+cube = num ** 3
+
+# Print results
+print("Square of", num, "is:", square)
+print("Cube of", num, "is:", cube)
